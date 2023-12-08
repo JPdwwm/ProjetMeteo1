97244df049c6b842847128ceea193a5bf730cdda
JPdwwm <gripacus.cvg@gmail.com>
2023-12-08 17:11:24 +0100

    Modification du gitignore

--- js/meteo.js
+++ js/meteo.js
@@ -1,30 +1,28 @@
+import { config } from './config.js';
 
-import config from './config.js';
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 
-
-
-
-async function checkWeather(city){
-    response = await fetch(apiUrl + city + `&appid=${config.apiKey}`)
-    var data = await response.json();
+async function checkWeather(city) {
+try {
+    const response = await fetch(apiUrl + city + `&appid=${config.apiKey}`);
+    const data = await response.json();
     console.log(data);
 
-    document.querySelector (".city").innerHTML = data.name;
-    document.querySelector (".temperature").innerHTML = Math.round(data.main.temp)  + "°";
-    document.querySelector (".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector (".wind").innerHTML = data.wind.speed + " km/h";
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temperature").innerHTML = Math.round(data.main.temp) + "°";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
     document.querySelector(".weather").style.display = "block";
-}
+} catch (error) {
+    console.error("Erreur lors de la récupération des données météorologiques :", error);
+}};
 
-
-searchBtn.addEventListener("click", ()=>{
+searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
 });
-
 
 searchBox.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
