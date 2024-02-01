f881c14fdc3b2f69680fc56106f0c634ab20bb55
JPdwwm <gripacus.cvg@gmail.com>
2024-02-01 19:10:14 +0100

    Ajout du fetch

--- js/meteo.js
+++ js/meteo.js
@@ -1,24 +1,28 @@
 import { config } from './config.js';
 
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 
-async function checkWeather(city) {
-try {
-    const response = await fetch(apiUrl + city + `&appid=${config.apiKey}`);
-    const data = await response.json();
-    console.log(data);
-
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temperature").innerHTML = Math.round(data.main.temp) + "°";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-
-    document.querySelector(".weather").style.display = "block";
-} catch (error) {
-    console.error("Erreur lors de la récupération des données météorologiques :", error);
-}};
+function checkWeather(city) {
+    fetch(config.apiUrl + city + `&appid=${config.apiKey}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP! Statut : ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+            document.querySelector(".city").innerHTML = data.name;
+            document.querySelector(".temperature").innerHTML = Math.round(data.main.temp) + "°";
+            document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+            document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+            document.querySelector(".weather").style.display = "block";
+        }
+    )
+    .catch(error => {
+        console.error("Erreur lors de la récupération des données météorologiques :", error);
+    });
+}
 
 searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
@@ -29,6 +33,7 @@ searchBox.addEventListener("keydown", (event) => {
         checkWeather(searchBox.value);
     }
 });
+
 
 
 
